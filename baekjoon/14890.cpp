409110637184f97106656f1cc245836e2a731415
royeom <eom2390@gmail.com>
2019-01-27 18:13:43 +0900

    5th week's solution

--- baekjoon/14890.cpp
+++ baekjoon/14890.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <cstring>
 #define MAX 101
 int map[MAX][MAX];
 int N, L;
 bool rowFinished[MAX];
 bool colFinished[MAX];
 int count = 0;
+bool check_visit[MAX] = {false, };
+
+int temp[MAX][MAX] ={0, };
+
+void rotate_matrix(){    
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<N; ++j){
+            temp[j][N-1-i] = map[i][j];
+        }
+    }
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<N; ++j){
+            map[i][j] = temp[i][j];
+        }
+    }
+}
 
 int main(){
     scanf("%d %d", &N, &L);
@@ -13,39 +30,42 @@ int main(){
             scanf("%d", &map[i][j]);
         }
     }
-    
-    for(int i=0; i<N; ++i){
-        bool flag = true; // flag가 1이면 도중에 문제가 생긴 것
-        if(map[i][0] > map[i][N-1]){ // 왼쪽부터 스타트
+    for(int x=0; x<2; ++x){ // 90도 회전시켜서 똑같이 한번 더 해주기 위함.
+        for(int i=0; i<N; ++i){
+            bool flag = true; // flag가 1이면 도중에 문제가 생긴 것
+            std::memset(check_visit, false, N+1);
             for(int j=0; j<N-1; ++j){
-                if(flag == false){
-                    break;
-                }
                 int diff = map[i][j] - map[i][j+1];
                 if(diff == 0)
                     continue;
                 else if(diff == 1){
                     for(int k =1; k<=L; ++k){
-                        if(j+k >= N){
+                        if(map[i][j] - map[i][j+k] != 1 || check_visit[j+k]){
                             flag = false;
                             break;
                         }
-                        if(map[i][j] - map[i][j+k] != 1){
-                            flag = false;
-                            break;
-                        }
+                        check_visit[j+k] = true;
                     }
-                }else if(diff >= 2){
+                }
+                else if(diff == -1){
+                    for(int k =0; k<L; ++k){
+                        if(map[i][j+1] - map[i][j-k] != 1 || check_visit[j-k]){
+                            flag = false;
+                            break;
+                        }
+                        check_visit[j-k] = true;
+                    }
+                }
+                else if(diff >= 2 || diff <=-2){
                     flag = false;
                     break;
+                    }
                 }
-            }
             if(flag == true)
                 count++; 
-        }else{ // 오른쪽부터 스타트
-            for(int j=N-1; j>=1; --j){
-                
-            }
         }
+        rotate_matrix();
     }
+    printf("%d\n", count);
+    return 0;
 }
